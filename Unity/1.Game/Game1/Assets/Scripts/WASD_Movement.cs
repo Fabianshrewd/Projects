e04a5ab6e24e5ac826af1201151a5d734332df30
Fabianshrewd <fabiangranig.fg@gmail.com>
2021-12-13 12:22:12 +0100

    Objekte erstellt und Movement angefangen

---
+++ Unity/1.Game/Game1/Assets/Scripts/WASD_Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASD_Movement : MonoBehaviour
+{
+    public float size = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject player = GameObject.Find("Player");
+        size += 0.1f;
+        player.transform.position = new Vector3(size, 0, 0);
+    }
+}
