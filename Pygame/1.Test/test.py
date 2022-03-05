0de99d3b2b99b42a23aec21fa41b1a250fd4c8e8
fabiang <fabiangranig.fg@gmail.com>
2022-03-05 16:05:05 -0500

    Pygame Test: Added borders and movement. Also startet with colliders.

--- Pygame/1.Test/test.py
+++ Pygame/1.Test/test.py
@@ -1,23 +1,70 @@
 import pygame
 
+#classe
+#enemie
+class Creature:
+    pos_x = int(0)
+    pos_y = int(0)
+
+#functions
+#function draw rectangle
+def Rectangle(screen, color, pos_x, pos_y, size_x, size_y):
+    pygame.draw.rect(screen, color, pygame.Rect(pos_x, pos_y, size_x, size_y))
+
+#function for the collider
+def Collider():
+    x = 1
+
+#Main Program
 #Start pygame
 pygame.init()
-
-#Make the screen
-screen = pygame.display.set_mode((400, 300))
+screen = pygame.display.set_mode((1280, 720))
 done = False
-
-#Make an rectangle
-pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(30, 30, 60, 60))
+clock = pygame.time.Clock()
+movement_x = int(10)
+movement_y = int(10)
 
 #Check if Pygame should be closed
 while not done:
     for event in pygame.event.get():
         if(event.type == pygame.QUIT):
             done = True
+    
+    #Loop
+    #Refill the screen
+    screen.fill((1, 0, 66))
+    
+    #Build a border
+    color_black = (0, 0 , 0)
+    Rectangle(screen, color_black, 0, 0, 1280, 10)
+    Rectangle(screen, color_black, 0, 0, 10, 720)
+    Rectangle(screen, color_black, 0, 710, 1280, 10)
+    Rectangle(screen, color_black, 1270, 0, 10, 720)
+    
+    #Collider
+    
 
     #Draw an rectangle
-    pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(30, 30, 60, 60))
+    color = (0, 128, 255)
+    Rectangle(screen, color, movement_x, movement_y, 100, 100)
+    
+    #Movement on Key
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_d]:
+        if movement_x < 1170:
+            movement_x += 1
+    if pressed[pygame.K_a]: 
+        if movement_x > 10:
+            movement_x -= 1
+    if pressed[pygame.K_s]: 
+        if movement_y < 610:
+            movement_y += 1
+    if pressed[pygame.K_w]: 
+        if movement_y > 10:
+            movement_y -= 1
+
+    #Make a clock so it runs slower
+    clock.tick(1000)
 
     #Update the window
     pygame.display.flip()
