aa7ce1f46cbefe6501189aa437c860e8e4f1377c
fabiang <fabiangranig.fg@gmail.com>
2022-03-05 08:40:52 -0500

    Added rectangle to the test.py

--- Pygame/1.Test/test.py
+++ Pygame/1.Test/test.py
@@ -7,11 +7,17 @@ pygame.init()
 screen = pygame.display.set_mode((400, 300))
 done = False
 
+#Make an rectangle
+pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(30, 30, 60, 60))
+
 #Check if Pygame should be closed
 while not done:
     for event in pygame.event.get():
         if(event.type == pygame.QUIT):
             done = True
 
-#Update the window
-pygame.display.flip()
+    #Draw an rectangle
+    pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(30, 30, 60, 60))
+
+    #Update the window
+    pygame.display.flip()
